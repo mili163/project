764a1725514dd6d3cd1f4958b95695a56887b46c
mili163 <miri18408@gmail.com>
2026-05-31 23:33:10 +0300

    Add files via upload

---
+++ backend/Program.cs
@@ -0,0 +1,28 @@
+using StudentSorter.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register services
+builder.Services.AddSingleton<IDataStore, InMemoryDataStore>();
+builder.Services.AddScoped<ICsvParserService, CsvParserService>();
+builder.Services.AddScoped<ISortingService, SortingService>();
+
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+app.MapControllers();
+app.Run();
